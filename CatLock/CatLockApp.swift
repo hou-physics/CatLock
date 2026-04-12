55d43e82e055c1b71f14a97aa83841b63b58e2f5
hou-physics <hou.physics@gmail.com>
2026-04-13 01:26:45 +0200

    Initial Commit

---
+++ CatLock/CatLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatLockApp.swift
+//  CatLock
+//
+//  Created by Zhengxu Hou on 13.04.26.
+//
+
+import SwiftUI
+
+@main
+struct CatLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
