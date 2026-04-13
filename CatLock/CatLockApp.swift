3625e46d8d61c8f65054d7e76270e74fe62bc2d0
hou-physics <hou.physics@gmail.com>
2026-04-13 02:57:11 +0200

    feat: rewrite CatLockApp as MenuBarExtra with AppDelegate

--- CatLock/CatLockApp.swift
+++ CatLock/CatLockApp.swift
@@ -1,17 +1,60 @@
-//
-//  CatLockApp.swift
-//  CatLock
-//
-//  Created by Zhengxu Hou on 13.04.26.
-//
-
 import SwiftUI
 
 @main
 struct CatLockApp: App {
+    @StateObject private var lockManager = LockManager.shared
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        MenuBarExtra {
+            MenuBarMenu(lockManager: lockManager)
+        } label: {
+            Image(systemName: lockManager.isLocked ? "lock.fill" : "lock.open.fill")
+        }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        LockManager.shared.setupShortcutMonitors()
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        LockManager.shared.unlock()
+    }
+}
+
+struct MenuBarMenu: View {
+    @ObservedObject var lockManager: LockManager
+
+    var body: some View {
+        if lockManager.isLocked {
+            Button("解锁") {
+                lockManager.unlock()
+            }
+        } else {
+            Button("锁定") {
+                lockManager.lock()
+            }
+            .disabled(!lockManager.hasAccessibility)
+        }
+
+        Divider()
+
+        Text("⌃⌥⌘L")
+            .foregroundColor(.secondary)
+
+        if !lockManager.hasAccessibility {
+            Divider()
+            Button("授权辅助功能权限...") {
+                AccessibilityHelper.showPermissionAlert()
+            }
+        }
+
+        Divider()
+
+        Button("退出 CatLock") {
+            NSApplication.shared.terminate(nil)
         }
     }
 }
